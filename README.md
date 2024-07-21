8ac4735e0499691342c6ab9165b8cb12e95b9752
Yuval Saraf <unimonkiez@gmail.com>
2024-07-21 13:09:12 +0300

    Some changes

---
+++ README.md
@@ -0,0 +1,1 @@
+# [redis-queues-exporter](https://hub.docker.com/r/r2wireless/redis-queues-exporter)
--- exporter/start.test.ts
+++
@@ -1,6 +0,0 @@
-import "jest";
-import {test, expect} from "bun:test";
-
-test("Example test", () => {
-  expect(1).toBe(1);
-});
